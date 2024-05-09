fba23ca93eaaee47e5ede20b90825eb9b7ba61dd
Kethireddy Ajay kumar Reddy <ajaykumarreddy7474@gmail.com>
2024-05-09 23:20:31 +0530

    first commit

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -1,30 +1,17 @@
-import { BellIcon, HomeIcon, SearchIcon } from '@heroicons/react/outline';
-import Image from 'next/image';
-import T7 from '../public/four4.jpg';
-
+import { HomeIcon } from '@heroicons/react/outline';
+import Cards from '../components/Cards';
+import Header from '../components/Header';
+import TableDashboard from '../components/TableDashboard';
 const Dashboard = () => {
   return (
-    <div className="bg-slate-200 min-h-screen ">
-
-
-<div className="flex justify-between items-center px-6 py-4 bg-slate-200 border-b-slate-300 border-2 ">
-        <div className="flex items-center gap-2 md:w-auto ">
-          <SearchIcon className="w-6 h-6 text-gray-500" />
-          <input type="text" placeholder="Search" className="relative font-semibold border-none px-2 bg-slate-200" />
-        </div>
-
-        <div className="flex items-center justify-center mt-4 md:mt-0">
-          <BellIcon className="w-6 h-6 text-gray-500 mr-2" />
-          <div className="h-8 w-8 relative">
-            <Image src={T7} alt="Profile Picture" layout="fill" className="rounded-full" />
-          </div>
-        </div>
-      </div>
-      <div className="w-full ">
-      <div className="flex flex-row items-center justify-start bg-gray-300 border-b-slate-200 p-1 md:mx-20 md:my-5 md:px-10 md:gap-4">
+    <div className=" ">
+<Header />
+{/* ------------------------------------ - ----------------------------------- */}  
+<div className="w-full ">
+      <div className="flex flex-row items-center justify-start bg-slate-200 border-b-slate-300 p-1 md:mx-20 md:my-5 md:px-10 md:gap-4">
         <div className="flex items-center gap-2 relative">
           <HomeIcon className="w-6 h-6" />
-        
+       
         </div>
         <div className="relative w-14 h-14 mx-3 bg-slate-100 clip-path-polygon"></div>
         <div className="flex items-center">
@@ -33,13 +20,13 @@ const Dashboard = () => {
         <div className="relative w-14 h-14 mx-3 bg-slate-100 clip-path-polygon"></div>
       </div>
     </div>
+    {/* ------------------------------------ - ----------------------------------- */}  
+  
+    <Cards/>
 
-<div className="px-20">
-    <div class="w-full h-96 bg-slate-300 border-dashed border-4 border-slate-400">
-      
+  <TableDashboard />
     </div>
-    </div>
-    </div>
+
   )
 }
 
